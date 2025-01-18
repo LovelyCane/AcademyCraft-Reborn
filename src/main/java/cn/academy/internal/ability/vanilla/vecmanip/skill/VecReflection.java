bbd7ff7cdba9d389ebf5c4d145fc27088dd8d734
LovelyCane <cane.sama.lovely@gmail.com>
2025-01-18 15:11:38 +0800

    A bug.

--- src/main/java/cn/academy/internal/ability/vanilla/vecmanip/skill/VecReflection.java
+++ src/main/java/cn/academy/internal/ability/vanilla/vecmanip/skill/VecReflection.java
@@ -1,12 +1,14 @@
 package cn.academy.internal.ability.vanilla.vecmanip.skill;
 
 import cn.academy.api.ability.Skill;
-import cn.academy.internal.ability.context.ClientRuntime;
+import cn.academy.internal.ability.context.*;
+import net.minecraft.util.ResourceLocation;
 
 public class VecReflection extends Skill {
     public static VecReflection INSTANCE = new VecReflection();
     public static final String MSG_EFFECT = "effect";
     public static final String MSG_REFLECT_ENTITY = "reflect_ent";
+    public static boolean activate;
 
     public VecReflection() {
         super("vec_reflection", 5);
@@ -14,6 +16,40 @@ public class VecReflection extends Skill {
 
     @Override
     public void activate(ClientRuntime rt, int keyID) {
-        activateSingleKey(rt, keyID, VecReflectionContext::new);
+        rt.addKey(keyID, new KeyDelegate() {
+            @Override
+            public void onKeyDown() {
+                Context<VecReflection> context = findContext();
+                if (context != null) {
+                    context.terminate();
+                } else {
+                    ContextManager.instance.activate(new VecReflectionContext(getPlayer()));
+                }
+            }
+
+            @Override
+            public ResourceLocation getIcon() {
+                return INSTANCE.getHintIcon();
+            }
+
+            @Override
+            public int createID() {
+                return 0;
+            }
+
+            @Override
+            public Skill getSkill() {
+                return INSTANCE;
+            }
+
+            @Override
+            public DelegateState getState() {
+                return findContext() != null ? DelegateState.ACTIVE : DelegateState.IDLE;
+            }
+
+            private Context<VecReflection> findContext() {
+                return ContextManager.instance.findLocal(VecReflectionContext.class).orElse(null);
+            }
+        });
     }
 }
